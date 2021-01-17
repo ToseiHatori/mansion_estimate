043c7e895748c572078e78cb7df955a0d2146f7f
hatry <tosei.hatori@gmail.com>
2021-01-17 11:59:43 +0900

    だいたい作成

--- src/main.py
+++ src/main.py
@@ -5,10 +5,13 @@ import subprocess
 import datetime
 import glob
 import re
+from jeraconv import jeraconv
 
+# 日付など
 dt_now = datetime.datetime.now()
 dt_now = dt_now.strftime("%Y%m%d_%H:%M")
 gc.enable()
+pd.options.display.max_columns = None
 
 # branch名の取得
 _cmd = "git rev-parse --abbrev-ref HEAD"
@@ -72,7 +75,71 @@ if __name__ == "__main__":
         # 物件情報、面積
         df["area"] = [float(x) if x != "2000㎡以上" else 2000 for x in df["面積（㎡）"]]
 
-        unuse_columns = ["都道府県名", "市区町村名", "地区名", "最寄駅：名称", "最寄駅：距離（分）", "間取り", "面積（㎡）"]
+        # 建築年
+        j2w = jeraconv.J2W()
+        y = [x if x != "戦前" else "昭和10年" for x in df["建築年"].fillna("不明")]
+        df["year_of_construction"] = [j2w.convert(x) if x != "不明" else None for x in y]
+
+        # 建物の構造
+        df["structure"] = df["建物の構造"]
+        df["structure_block"] = [int("ブロック造" in x) for x in df["建物の構造"].fillna("")]
+        df["structure_wood"] = [int("木造" in x) for x in df["建物の構造"].fillna("")]
+        df["structure_lightiron"] = [int("軽量鉄骨造" in x) for x in df["建物の構造"].fillna("")]
+        df["structure_iron"] = [int("鉄骨造" in x) for x in df["建物の構造"].fillna("")]
+        df["structure_RC"] = [int("ＲＣ" in x) for x in df["建物の構造"].fillna("")]  # SRCも含まれるけどいいのか
+        df["structure_SRC"] = [int("ＳＲＣ" in x) for x in df["建物の構造"].fillna("")]
+
+        # 用途
+        df["usage"] = df["用途"]
+        df["usage_sonota"] = [int("その他" in x) for x in df["用途"].fillna("")]
+        df["usage_office"] = [int("事務所" in x) for x in df["用途"].fillna("")]
+        df["usage_warehouse"] = [int("倉庫" in x) for x in df["用途"].fillna("")]
+        df["usage_shop"] = [int("店舗" in x) for x in df["用途"].fillna("")]
+        df["usage_parking"] = [int("駐車場" in x) for x in df["用途"].fillna("")]
+        df["usage_house"] = [int("住宅" in x) for x in df["用途"].fillna("")]
+        df["usage_workshop"] = [int("作業場" in x) for x in df["用途"].fillna("")]
+        df["usage_factory"] = [int("工場" in x) for x in df["用途"].fillna("")]
+
+        # 今後の利用目的、都市計画、建ぺい率、改装
+        df["future_usage"] = df["今後の利用目的"]
+        df["city_plan"] = df["都市計画"]
+        df["building_coverage_ratio"] = df["建ぺい率（％）"].astype(float)
+        df["floor_area_ratio"] = df["容積率（％）"].astype(float)
+        df["remodeling"] = df["改装"]
+
+        # 取引時期など
+        df["base_year"] = [float(x[0:4]) for x in df["取引時点"]]
+        df["base_quarter"] = [float(x[6:7]) for x in df["取引時点"]]
+        df["passed_year"] = df["base_year"] - df["year_of_construction"]
+
+        # 取引の事情等
+        df["reason"] = df["取引の事情等"]
+        df["reason_other"] = [int("その他事情有り" in x) for x in df["取引の事情等"].fillna("")]
+        df["reason_burden"] = [int("他の権利・負担付き" in x) for x in df["取引の事情等"].fillna("")]
+        df["reason_auction"] = [int("調停・競売等" in x) for x in df["取引の事情等"].fillna("")]
+        df["reason_defects"] = [int("瑕疵有りの可能性" in x) for x in df["取引の事情等"].fillna("")]
+        df["reason_related_parties"] = [int("関係者間取引" in x) for x in df["取引の事情等"].fillna("")]
+
+        # 削除
+        unuse_columns = [
+            "都道府県名",
+            "市区町村名",
+            "地区名",
+            "最寄駅：名称",
+            "最寄駅：距離（分）",
+            "間取り",
+            "面積（㎡）",
+            "建築年",
+            "建物の構造",
+            "用途",
+            "今後の利用目的",
+            "都市計画",
+            "建ぺい率（％）",
+            "容積率（％）",
+            "改装",
+            "取引時点",
+            "取引の事情等",
+        ]
         df = df.drop(unuse_columns, axis=1)
 
         logger.debug(f"head : {df.head()}")
