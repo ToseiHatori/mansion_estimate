9854bace0548dd90125e38eab05c39441effdb9a
hatry <tosei.hatori@gmail.com>
2021-02-20 12:51:55 +0900

    mem

--- src/main.py
+++ src/main.py
@@ -114,6 +114,40 @@ def set_seed(seed):
 
 
 @Cache("./cache")
+def reduce_mem_usage(df, logger=None, level=logging.DEBUG):
+    print_ = tprint if logger is None else lambda msg: logger.log(level, msg)
+    start_mem = df.memory_usage().sum() / 1024 ** 2
+    print_("Memory usage of dataframe is {:.2f} MB".format(start_mem))
+
+    for col in df.columns:
+        col_type = df[col].dtype
+        if col_type != "object" and col_type != "datetime64[ns]":
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == "int":
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+
+    end_mem = df.memory_usage().sum() / 1024 ** 2
+    print_("Memory usage after optimization is: {:.2f} MB".format(end_mem))
+    print_("Decreased by {:.1f}%".format(100 * (start_mem - end_mem) / start_mem))
+    return df
+
+
+@Cache("./cache")
 def get_data(debug):
     unuse_columns = ["種類", "地域", "市区町村コード", "土地の形状", "間口", "延床面積（㎡）", "前面道路：方位", "前面道路：種類", "前面道路：幅員（ｍ）"]
     train_files = sorted(glob.glob("./data/raw/train/*"))
@@ -434,7 +468,6 @@ class LGBTrainer(GroupKfoldTrainer):
             dtrain,
             valid_sets=[dtrain, dvalid],
             num_boost_round=50000,
-            categorical_feature=self.categorical_cols,
             early_stopping_rounds=100,
             verbose_eval=100,
         )
@@ -785,6 +818,9 @@ if __name__ == "__main__":
     (train_df, test_df, sample_submission) = get_data(debug)
     tprint("preprocessing data")
     (train_df, test_df) = preprocess(train_df, test_df)
+    tprint("reduce memory usage")
+    train_df = reduce_mem_usage(train_df)
+    test_df = reduce_mem_usage(test_df)
     if debug:
         train_df = train_df.sample(1000, random_state=100).reset_index(drop=True)
     predictors = [
