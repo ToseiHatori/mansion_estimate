e2191ae877d15808e536057c0f48c6c566a77986
hatry <tosei.hatori@gmail.com>
2021-03-10 21:28:43 +0900

    update

--- src/main.py
+++ src/main.py
@@ -294,11 +294,13 @@ def preprocess(train_df, test_df):
     # NN系の処理をすることを見越してcopyしておく
     df_nn = df.copy()
     # 掛け算変数作成
+    unuse_cols = ["base_quarter", "timing_code", "null_num", "plan_num"]
+    numeric_cols_times = [x for x in numeric_cols if x not in unuse_cols]
     encoder = Pipeline(
         [
             SelectNumerical(),
             ArithmeticCombinations(
-                input_cols=numeric_cols,
+                input_cols=numeric_cols_times,
                 drop_origin=True,
                 output_suffix="_times",
                 operator="*",
